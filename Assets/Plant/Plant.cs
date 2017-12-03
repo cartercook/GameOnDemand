86d5258bd4ef31513dca429a98a20854aee7f8e3
Carter Cook <13cjcc@gmail.com>
2017-12-03 18:36:24 -0500

    beginings of plant animation

---
+++ Assets/Plant/Plant.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plant : MonoBehaviour {
+
+    // 0 = hole
+    // 1 = sprout stage 1
+    // 2 = sprout stage 2
+    public Sprite[] sprites;
+
+    new SpriteRenderer renderer;
+
+	// Use this for initialization
+	void Start () {
+        renderer = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
