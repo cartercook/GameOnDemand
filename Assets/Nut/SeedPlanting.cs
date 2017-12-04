bfd4f22a17a6502662cc608246202acdba0bbc38
BastienEscaich <b.escaich@gmail.com>
2017-12-03 21:46:56 -0500

    Fixed seeds quantity Tried to fix planting

--- Assets/Nut/SeedPlanting.cs
+++ Assets/Nut/SeedPlanting.cs
@@ -2,18 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SeedPlanting : MonoBehaviour {
+public class SeedPlanting : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("mode: " + Controls.mode);
+
         if (Controls.mode == Controls.Mode.planting)
         {
-            foreach (Touch touch in Controls.GetTouchesAndMouse())
+            Touch[] touches = Controls.GetTouchesAndMouse();
+
+            print("touches: " + touches.Length);
+
+            foreach (Touch touch in touches)
             {
                 Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
 
@@ -22,37 +31,55 @@ public class SeedPlanting : MonoBehaviour {
                     // Drag
                     transform.position = touchPos;
                 }
-                else
+                else if (touch.phase == TouchPhase.Ended)
                 {
                     LandTile tile = Controls.GetComponentAtPos<LandTile>(transform.position, "Tile");
 
-                    print("Tile: " + tile);
-
-                    if (tile != null)
+                    if (tile != null && tile.status == LandTile.Status.tilled)
                     {
-                        if (tile.status == LandTile.Status.tilled)
-                        {
-                            transform.position = (Vector2)tile.transform.position;
+                        transform.position = (Vector2)tile.transform.position;
+                    }
+                }
+            }
 
-                            // make seed smaller until it disappears
-                            transform.localScale -= new Vector3(0.07f, 0.07f, 0);
+            if (touches.Length <= 0)
+            {
+                LandTile tile = Controls.GetComponentAtPos<LandTile>(transform.position, "Tile");
 
-                            if (transform.localScale.x < 0.1f && transform.localScale.y < 0.1f)
-                            {
-                                print("destroy1");
-                                tile.PlantTile();
-                                Controls.mode = Controls.Mode.gathering_seeds;
-                                Destroy(gameObject);
-                            }
-                        }
-                        else
+                print("Tile: " + tile);
+
+                if (tile != null)
+                {
+                    print("Tile: " + tile.status);
+
+                    if (tile.status == LandTile.Status.tilled)
+                    {
+                        // make seed smaller until it disappears
+                        transform.localScale -= new Vector3(0.07f, 0.07f, 0);
+
+                        print("Tile: " + tile.status);
+
+                        print("Tile: " + transform.position);
+
+                        print("Tile x: " + transform.localScale.x);
+                        print("Tile y: " + transform.localScale.y);
+
+                        if (transform.localScale.x < 0.1f && transform.localScale.y < 0.1f)
                         {
-                            print("destroy2");
+                            print("destroy1");
+                            tile.PlantTile();
+                            Controls.mode = Controls.Mode.gathering_seeds;
                             Destroy(gameObject);
                         }
+                    }
+                    else
+                    {
+                        print("destroy2");
+                        Controls.mode = Controls.Mode.gathering_seeds;
+                        Destroy(gameObject);
                     }
                 }
             }
         }
-	}
+    }
 }
